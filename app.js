712552b5f39efaeac0672c78a358a9ade08f1d85
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-09-09 18:22:45 +0200

    final push

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const Vonage = require("@vonage/server-sdk");
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
   cors: {
@@ -79,34 +80,75 @@ app.get("/addclient", (req, res) => {
   }
 });
 
-app.get("/removeclient", (req, res) => {
+app.get("/removeclient", async (req, res) => {
   const { value } = req.query;
-  if (value) {
-    if (value == true || value == 1) {
-      const io = req.app.get("socketio");
-      const date = new Date();
-      Axios.get(process.env.API_URL + "/removeclient/?sd=" + date)
-        .then((res) => {
-          console.log(res.data);
-        })
-        .catch((error) => console.log(error.message));
-      io.emit("removeClient", "");
-      return res.status(400).send("User removed from the queue!");
+  try {
+    if (value) {
+      if (value == true || value == 1) {
+        const io = req.app.get("socketio");
+        const date = new Date();
+        const result = await Axios.get(
+          process.env.API_URL + "/removeclient/?sd=" + date
+        );
+        // 0780515771
+        console.log(result.data);
+        if (result.data.length > 0) {
+          await sendSMS(result.data);
+        }
+        io.emit("removeClient", "");
+        return res.status(400).send("User removed from the queue!");
+      } else {
+        return res.status(401).send("No user to remove from the queue");
+      }
     } else {
-      return res.status(401).send("No user to remove from the queue");
+      return res.status(400).send("Invalid request");
     }
-  } else {
-    return res.status(400).send("Invalid request");
+  } catch (error) {
+    console.log(error.message);
   }
 });
+
+const sendSMS = async (users) => {
+  for (let i = 0; i < users.length; i++) {
+    try {
+      const vonage = new Vonage({
+        apiKey: "72bff39b",
+        apiSecret: "Q9bv0bL1MilNuD7E",
+      });
+
+      const from = "Bank Queue";
+      const to = "25" + users[i].phone;
+      const text = `Dear user, now the queue length is at ${users[i].queueLength}`;
+
+      await vonage.message.sendSms(from, to, text, (err, responseData) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (responseData.messages[0]["status"] === "0") {
+            console.log("Message sent successfully.");
+          } else {
+            console.log(
+              `Message failed with error: ${responseData.messages[0]["error-text"]}`
+            );
+          }
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  try {
+    await Axios.get(
+      process.env.API_URL +
+        "/updatenotifications/?length=" +
+        users[0].queueLength
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
-
-// user Object
-// {
-//   date joined
-//   usename: anonymous
-// }
